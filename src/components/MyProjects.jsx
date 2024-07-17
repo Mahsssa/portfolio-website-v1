b65563513a1049b9930c5b4c0ce0cae652535cf9
Mahsa <m73.keshavarz@gmail.com>
2024-07-17 23:58:28 +0330

    Added projects section

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -1,7 +1,95 @@
 import React from "react";
+import { styles } from "../styles";
+import { github, demo } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { motion } from "framer-motion";
+import { fadeIn, textVariant } from "../utils/motion";
 
-const MyProjects = () => {
-  return <div>My projects</div>;
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+  demo_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.6, 1)}>
+      <div className="bg-tertiary p-4 sm:p-6 rounded-xl w-full sm:w-[550px]  border border-slate-800">
+        <div className="relative w-full h-[200px]">
+          <img
+            src={image}
+            alt="project_image"
+            className="w-full h-full object-cover rounded-xl"
+          />
+          {source_code_link !== "" && (
+            <div className="github absolute inset-0 flex justify-end m-1 card-img_hover">
+              <div
+                onClick={() => window.open(source_code_link, "_blank")}
+                className="bg-tertiary w-9 h-9 rounded-full flex justify-center items-center cursor-pointer"
+              >
+                <img
+                  src={github}
+                  alt="source code"
+                  className="w-5/6 h-5/6 object-contain"
+                />
+              </div>
+            </div>
+          )}
+
+          {demo_link !== "" && (
+            <div className="live-demo absolute inset-1 flex justify-end me-11 card-img_hover">
+              <div
+                onClick={() => window.open(demo_link, "_blank")}
+                className="bg-tertiary w-9 h-9 rounded-full flex justify-center items-center cursor-pointer"
+              >
+                <img
+                  src={demo}
+                  alt="source code"
+                  className="w-5/6 h-5/6 object-contain"
+                />
+              </div>
+            </div>
+          )}
+        </div>
+
+        <div className="mt-5 mb-4">
+          <h3 className="text-cyan font-bold text-[20px] sm:text-[22px] md:text-[24px]">
+            {name}
+          </h3>
+          <p className={`${styles.cardSubText} mt-2 text-slate-300`}>
+            {description}
+          </p>
+        </div>
+
+        <div className="mb-2 flex flex-wrap gap-3">
+          {tags.map((tag) => (
+            <p key={`${name}-${tag.name}`} className="text-[14px] text-cyan">
+              {tag.name}
+            </p>
+          ))}
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
-export default MyProjects;
+const MyProjects = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <h2 className={styles.sectionHeadText}>Projects</h2>{" "}
+      </motion.div>
+
+      <div className="mt-10 flex flex-wrap gap-9">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(MyProjects, "Projects");
