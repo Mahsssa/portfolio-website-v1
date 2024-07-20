ed6991b5cea4924c89ed14234163f4bfbaf07413
Mahsa <m73.keshavarz@gmail.com>
2024-07-20 11:35:59 +0330

    Added screen loader

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,28 @@ import {
   MyProjects,
   Footer,
 } from "./components";
+import React, { useState, useEffect } from "react";
+import LoadingScreen from "./components/LoadingScreen";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3000);
+  }, []);
   return (
     <>
+      {loading && <LoadingScreen />}
       <BrowserRouter>
         <div className="relative z-0 bg-primary">
-          <Navbar />
-          <Hero />
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
           <About />
           <Tech />
+
           <Experience />
+
           <MyProjects />
           <Contact />
           <Footer />
