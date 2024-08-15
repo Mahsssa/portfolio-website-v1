19877bb85b6630b366501c7fb42f3c40a5b58b6e
Mahsa <m73.keshavarz@gmail.com>
2024-08-15 12:20:38 +0330

    Refactored screen loader styles for consistency.

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import "/src/screenLoader.css";
 
 const LoadingScreen = () => {
   return (
-    <div className="loading-screen">
-      <div className="loading-spinner"></div>
+    <div className="fixed inset-0 flex justify-center items-center bg-primary z-[9999]">
+      <div className="relative w-[85px] h-[85px]">
+        <div className="absolute inset-0 rounded-full shadow-[0_0_8px_2px_rgba(0,0,0,0.4)_inset]"></div>
+        <div className="absolute inset-0 rounded-full shadow-[0_3px_0_#42a5a7_inset] animate-spin"></div>
+      </div>
     </div>
   );
 };
